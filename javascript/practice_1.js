92e23c04d2dd77cd23a048bc798ef6fd6f954308
fumtas1k <fumta01a7@gmail.com>
2024-05-03 22:50:47 +0900

    [add]practiceAのruby, javascript追加

---
+++ javascript/practice_1.js
@@ -0,0 +1,7 @@
+// PracticeA - Welcome to AtCoder
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').split("\n");
+const A = parseInt(input[0], 10);
+const [B, C] = input[1].split(" ").map(n => parseInt(n, 10));
+const S = input[2];
+console.log(A + B + C, S);
---
+++ ruby/practice_1.rb
@@ -0,0 +1,6 @@
+# PracticeA - Welcome to AtCoder
+
+A = gets.to_i
+B, C = gets.split.map(&:to_i)
+S = gets.chomp
+puts [A + B + C, S].join(" ")
