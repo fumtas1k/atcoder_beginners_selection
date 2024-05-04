3950b55fb1473c1be09f8ac4844dddfba2e80dc7
fumtas1k <fumta01a7@gmail.com>
2024-05-04 23:45:19 +0900

    [fix]doc修正

--- javascript/abc088_b.js
+++ javascript/abc088_b.js
@@ -2,7 +2,7 @@
 
 const input = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
 const N = parseInt(input[0], 10);
-// sort().reverse()にすると半分が WA になるため、reverse を使用していない
+// sort()は、文字列としてソートするため比較関数を指定する必要がある。
 const A = input[1].split(" ").map(n => parseInt(n, 10)).sort((a, b) => b - a);
 const ans = A.reduce((acc, a, i) => acc + (i % 2 === 0 ? a : -a), 0);
 console.log(ans);
