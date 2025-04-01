db378a9e1c055909a0efe0574cff52a9e0ece4b3
fumtas1k <fumta01a7@gmail.com>
2025-04-01 23:58:55 +0900

    [improve]少し高速化

--- haskell/abc087_b.hs
+++ haskell/abc087_b.hs
@@ -1,8 +1,8 @@
 -- ABC087B - Coins
 
-import Control.Monad
+import Control.Monad (replicateM)
 
 main :: IO ()
 main = do
   [a, b, c, x] <- replicateM 4 (readLn :: IO Integer)
-  print $ length [() | i <- [0 .. a], j <- [0 .. b], k <- [0 .. c], 500 * i + 100 * j + 50 * k == x]
+  print $ sum [ 1 | i <- [0 .. a], j <- [0 .. b], k <- [0 .. c], 500 * i + 100 * j + 50 * k == x]
